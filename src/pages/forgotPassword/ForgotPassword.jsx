7ad697b6031a991ecad7261ad2a906ffc800ef24
Rishad Alam <sazzadullalamrishad@gmail.com>
2023-07-23 02:22:15 +0600

    feat: forgot password error message complete

--- src/pages/forgotPassword/ForgotPassword.jsx
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import "../login/login.scss"
+import { create } from "mutative";
 
 export default function ForgotPassword() {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState({
+        "email": ''
+    });
+
+    const setChange = (val) => {
+        setEmail(val)
+        setError(prevError => create(prevError, (draftErrors) => {
+            val === '' ? draftErrors.email = "Email Address is required!" : delete draftErrors.email
+            if (val !== '') {
+                !(/\S+@\S+\.\S+/.test(val)) ? draftErrors.email = "Email is invalid!" : delete draftErrors.confirmPassword
+            }
+        }))
+    }
+
     return (
         <>
             <div className="login p-5">
                 <form className="text-center">
                     <h2 className="mb-4">Find Your Accoun & Reset Password</h2>
-                    <input type="email" id="defaultLoginFormEmail" className="form-control mb-4" placeholder="Email Address" />
-                    <button className="btn btn-info btn-block" type="submit">Send Passsword Reset OTP</button>
+                    <input type="email" id="defaultLoginFormEmail" className={`form-control ${error.email ? " is-invalid" : ""}`} placeholder="Email Address" value={email || ''} onChange={e => setChange(e.target.value)} />
+                    {error.email &&
+                        <div class="invalid-feedback text-start">
+                            {error.email}
+                        </div>
+                    }
+                    <button className="btn btn-primary btn-block mt-4" type="submit" disabled={Object.keys(error).length}>Send Passsword Reset OTP</button>
                 </form>
             </div>
         </>
