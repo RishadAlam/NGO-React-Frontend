9fac42d02c67c7c01e44b25d2547b8a305c463ea
Rishad Alam <sazzadullalamrishad@gmail.com>
2023-07-27 00:17:48 +0600

    chore: Eslint reconfig

--- src/pages/forgotPassword/ForgotPassword.jsx
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -1,26 +1,22 @@
-import { useState } from "react"
-import FGPassword from "../../components/forgotPassword/ForgotPassword"
-import OtpVerification from "../../components/otpVerification/OtpVerification"
-import ResetPassword from "../../components/resetPassword/ResetPassword"
-import "../login/login.scss"
+import { useState } from 'react'
+import FGPassword from '../../components/forgotPassword/ForgotPassword'
+import OtpVerification from '../../components/otpVerification/OtpVerification'
+import ResetPassword from '../../components/resetPassword/ResetPassword'
+import '../login/login.scss'
 
 export default function ForgotPassword() {
-    const [email, setEmail] = useState("")
-    const [step, setStep] = useState(1)
+  const [email, setEmail] = useState('')
+  const [step, setStep] = useState(1)
 
-    const nextPage = (pageNo) => {
-        console.log(pageNo)
-    }
+  const nextPage = (pageNo) => {
+    console.log(pageNo)
+  }
 
-    return (
-        <>
-            {step === 1 && (
-                <FGPassword email={email} setEmail={setEmail} setStep={setStep} />
-            )}
-            {step === 2 && <OtpVerification setStep={setStep} />}
-            {step === 3 && (
-                <ResetPassword email={email} setEmail={setEmail} setStep={setStep} />
-            )}
-        </>
-    )
+  return (
+    <>
+      {step === 1 && <FGPassword email={email} setEmail={setEmail} setStep={setStep} />}
+      {step === 2 && <OtpVerification setStep={setStep} />}
+      {step === 3 && <ResetPassword email={email} setEmail={setEmail} setStep={setStep} />}
+    </>
+  )
 }
