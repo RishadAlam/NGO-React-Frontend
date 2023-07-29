feb43a7b5713858485c307a56d2d67e509ab0e1b
Rishad Alam <sazzadullalamrishad@gmail.com>
2023-07-29 15:51:23 +0600

    feat: PUT request added

--- src/utilities/xFetch.js
+++ src/utilities/xFetch.js
@@ -114,3 +114,50 @@ export async function postRequest(endpoint, data, queryParam = null) {
 
   return response
 }
+
+export async function putRequest(endpoint, data, queryParam = null) {
+  const uri = new URL(`/api/${endpoint}`, import.meta.env.VITE_BASE_URI)
+  const body = data instanceof FormData ? data : JSON.stringify(data)
+  // append query params in url
+  if (queryParam) {
+    for (const key in queryParam) {
+      if (key) {
+        uri.searchParams.append(key, queryParam[key])
+      }
+    }
+  }
+
+  const response = await axios
+    .put(uri, body, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Accept-Language': 'bn'
+      }
+    })
+    .then((res) => {
+      // console.log(res.data)
+      return res.data
+    })
+    .catch((errors) => {
+      if (errors.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        // console.log(errors.response.data)
+        // console.log(errors.response.status)
+        // console.log(errors.response.headers)
+        return errors.response.data
+      } else if (errors.request) {
+        // The request was made but no response was received
+        // `errors.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        return errors.request
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        // console.log('Error', errors.message)
+        return { success: false, errors: { message: errors.message } }
+      }
+    })
+
+  return response
+}
