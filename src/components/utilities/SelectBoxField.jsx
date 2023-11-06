e45f1255a84ac7781c7f8fa837eac2190f78e3f0
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2023-11-06 21:09:35 +0600

    fix: category config autocomplete with fixed

--- src/components/utilities/SelectBoxField.jsx
+++ src/components/utilities/SelectBoxField.jsx
@@ -18,19 +18,21 @@ export default function SelectBoxField({
 
   return (
     <>
-      <Autocomplete
-        {...config}
-        id="combo-box-demo"
-        disabled={disabled ? true : false}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label={isRequired ? requiredLabel : label}
-            variant={variant || 'standard'}
-          />
-        )}
-      />
-      {error && <span className="text-danger my-3">{error}</span>}
+      <div style={{ minWidth: '150px' }}>
+        <Autocomplete
+          {...config}
+          id="combo-box-demo"
+          disabled={disabled ? true : false}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label={isRequired ? requiredLabel : label}
+              variant={variant || 'standard'}
+            />
+          )}
+        />
+        {error && <span className="text-danger my-3">{error}</span>}
+      </div>
     </>
   )
 }
