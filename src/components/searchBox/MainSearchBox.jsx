b28d182807054bdff6d97ef42ec956028ac39c7f
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-01-30 18:22:27 +0600

    feat: Live search complete

--- src/components/searchBox/MainSearchBox.jsx
+++ src/components/searchBox/MainSearchBox.jsx
@@ -1,21 +1,71 @@
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+import { Link } from 'react-router-dom'
+import { useAuthDataValue } from '../../atoms/authAtoms'
+import { isEmpty } from '../../helper/isEmpty'
+import { isEmptyArray } from '../../helper/isEmptyObject'
 import Search from '../../icons/Search'
+import debounce from '../../libs/debounce'
+import tsNumbers from '../../libs/tsNumbers'
+import xFetch from '../../utilities/xFetch'
 import Button from '../utilities/Button'
 import './searchBox.scss'
 
 export default function MainSearchBox({ t }) {
+  const { accessToken } = useAuthDataValue()
+  const [searchKey, setSearchKey] = useState()
+  const [searchData, setSearchData] = useState([])
+
+  const liveSearch = debounce((e) => {
+    if (isEmpty(e.target.value)) {
+      setSearchData([])
+      return
+    }
+
+    xFetch('client/registration', null, null, accessToken, { search: e.target.value })
+      .then((response) => {
+        if (response?.success) {
+          setSearchData(response.data)
+          return
+        }
+      })
+      .catch((errResponse) => {
+        if (!errResponse?.errors) {
+          toast.error(errResponse?.message)
+        }
+      })
+  }, 500)
+
   return (
     <>
-      <div className="mainSearchBox">
+      <div className="mainSearchBox position-relative">
         <form>
           <input
             type="text"
             className="form-control form-input"
             placeholder={t('common.search_placeholder')}
+            onKeyUp={liveSearch}
           />
           <span className="left-pan">
             <Button name={<Search size={20} />} disabled={false} loading={false} type="button" />
           </span>
         </form>
+        {!isEmptyArray(searchData) && (
+          <div className={`search-box-dropdown position-absolute rounded-5`}>
+            <ul className="mb-0 p-3 shadow rounded">
+              {searchData.map((account) => (
+                <li key={account.id} className="pb-3 border-bottom">
+                  <Link to={`/client-register/${account.id}`} onClick={() => setSearchData([])}>
+                    <div className="dropdown-info">
+                      <b className="pe-2 border-end">{tsNumbers(account.acc_no)}</b>
+                      <small className="ms-2 text-nowrap">{account.name}</small>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </>
   )
