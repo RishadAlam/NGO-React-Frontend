628872d48a0d0ebff6392aca889ca90295f3f213
Rishad Alam <101513331+RishadAlam@users.noreply.github.com>
2024-05-26 21:26:34 +0600

    fix: DateRangePicker issue fixed

--- src/components/utilities/DateRangePickerInputField.jsx
+++ src/components/utilities/DateRangePickerInputField.jsx
@@ -1,4 +1,4 @@
-import { addDays, endOfDay, subDays } from 'date-fns'
+import { addDays, endOfDay, startOfDay, subDays } from 'date-fns'
 import { useTranslation } from 'react-i18next'
 import { DateRangePicker } from 'rsuite'
 import getCurrentMonth from '../../libs/getCurrentMonth'
@@ -14,28 +14,28 @@ export default function DateRangePickerInputField({
   const Ranges = [
     {
       label: t('common.today'),
-      value: [new Date().setUTCHours(0, 0, 0, 0), endOfDay(new Date())],
+      value: [startOfDay(new Date()), endOfDay(new Date())],
       placement: 'left'
     },
     {
       label: t('common.yesterday'),
-      value: [addDays(new Date(), -1).setUTCHours(0, 0, 0, 0), endOfDay(addDays(new Date(), -1))],
+      value: [startOfDay(addDays(new Date(), -1)), endOfDay(addDays(new Date(), -1))],
       placement: 'left'
     },
     {
       label: t('common.last_7days'),
-      value: [subDays(new Date(), 6).setUTCHours(0, 0, 0, 0), endOfDay(new Date())],
+      value: [startOfDay(subDays(new Date(), 6)), endOfDay(new Date())],
       placement: 'left'
     },
     {
       label: t('common.last_30days'),
-      value: [subDays(new Date(), 29).setUTCHours(0, 0, 0, 0), endOfDay(new Date())],
+      value: [startOfDay(subDays(new Date(), 29)), endOfDay(new Date())],
       placement: 'left'
     },
     {
       label: t('common.last_month'),
       value: [
-        new Date(new Date().getFullYear(), new Date().getMonth() - 1, 2).setUTCHours(0, 0, 0, 0),
+        startOfDay(new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1)),
         endOfDay(new Date(new Date().getFullYear(), new Date().getMonth(), 0))
       ],
       placement: 'left'
@@ -43,20 +43,20 @@ export default function DateRangePickerInputField({
     {
       label: t('common.this_month'),
       value: [
-        new Date(new Date().getFullYear(), new Date().getMonth(), 2).setUTCHours(0, 0, 0, 0),
+        startOfDay(new Date(new Date().getFullYear(), new Date().getMonth(), 1)),
         endOfDay(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0))
       ],
       placement: 'left'
     },
     {
       label: t('common.last_365days'),
-      value: [subDays(new Date(), 364).setUTCHours(0, 0, 0, 0), endOfDay(new Date())],
+      value: [startOfDay(subDays(new Date(), 364)), endOfDay(new Date())],
       placement: 'left'
     },
     {
       label: t('common.last_year'),
       value: [
-        new Date(new Date().getFullYear() - 1, 0, 2).setUTCHours(0, 0, 0, 0),
+        startOfDay(new Date(new Date().getFullYear() - 1, 0, 1)),
         endOfDay(new Date(new Date().getFullYear() - 1, 11, 31))
       ],
       placement: 'left'
@@ -64,7 +64,7 @@ export default function DateRangePickerInputField({
     {
       label: t('common.this_year'),
       value: [
-        new Date(new Date().getFullYear(), 0, 2).setUTCHours(0, 0, 0, 0),
+        startOfDay(new Date(new Date().getFullYear(), 0, 1)),
         endOfDay(new Date(new Date().getFullYear(), 11, 31))
       ],
       placement: 'left'
